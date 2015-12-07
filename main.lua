ed3f453c29ef672a31c3622378df7af306cf565b
Nander Voortman <nander@nander.net>
2015-12-08 00:50:00 +0100

    simple test

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ function findSolidTiles(map)
     local collidable_tiles = {}
     local layer = map.layers["collision"]
     for y = 1, map.height do
-    	for x = 1, map.width do
+      for x = 1, map.width do
 
         if layer.data[y][x] then
           collidable_tiles[#collidable_tiles] = addBlock((x-1)*tile_width,(y-1)*tile_height,tile_width,tile_height,gamestate)
@@ -29,24 +29,41 @@ function findSolidTiles(map)
     return collidable_tiles
 end
 function love.load()
-	gamestate.map = sti.new("map.lua")
-	  gamestate.cam = gamera.new(0,0,2000,2000)
+  love.graphics.setDefaultFilter( 'nearest', 'nearest' )
+  gamestate.map = sti.new("map.lua")
+    gamestate.cam = gamera.new(0,-100,2000,2000)
 i = 0
+  gamestate.me={x=0,y=0}
+
   findSolidTiles(gamestate.map)
 end
 
 function love.update(dt)
-	gamestate.cam:setPosition(400+i,200)
-	i = i + dt*60
+  gamestate.cam:setPosition(400+gamestate.me.x,300+gamestate.me.y)
+  if love.keyboard.isDown("left") then
+                gamestate.me.x = gamestate.me.x - dt*60
+              end
+                if love.keyboard.isDown("right") then
+                gamestate.me.x = gamestate.me.x + dt*60
+              end
+
+if love.keyboard.isDown("up") then
+
+              gamestate.me.y = gamestate.me.y + dt*60
+        end
+        if love.keyboard.isDown("down") then
+
+              gamestate.me.y = gamestate.me.y - dt*60
+        end
 end
 
 function love.draw(dt)
-	gamestate.cam:draw(function(l,t,w,h)
+  gamestate.cam:draw(function(l,t,w,h)
 gamestate.map:draw()
 end)
-	   love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+     love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 
-	
+  
 end
 function love.threaderror(thread, errorstr)
   print("Thread error!\n"..errorstr)
